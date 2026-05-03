3d4425aa97bb3cecd0dfd14042078500092494fd
Eduardo Flores <edfloreshz@gmail.com>
2026-05-03 02:40:23 -0700

    v0.2.1

--- flatpak/README.md
+++
@@ -1,8 +0,0 @@
-# Flatpak sources generation
-
-```
-python3 -m venv .venv
-source .venv/bin/activate.fish
-pip install -r requirements.txt
-python3 flatpak-cargo-generator.py ../Cargo.lock
-```
--- src/app/core/init.rs
+++ src/app/core/init.rs
@@ -57,7 +57,7 @@ impl AppModel {
         widget::about::About::default()
             .name(fl!("tasks"))
             .icon(widget::icon::from_name("dev.edfloreshz.Tasks"))
-            .version("0.2.0")
+            .version("0.2.1")
             .author("Eduardo Flores")
             .license("GPL-3.0-only")
             .links([
